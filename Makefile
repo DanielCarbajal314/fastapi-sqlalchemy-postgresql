4b347c5a10c5cfad78eaac81c1c6cd24ca63bf3b
Daniel Carbajal <danielcarbajal@Daniels-MacBook-Pro.local>
2024-05-08 00:07:24 -0500

    IaC Complete

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
+define get_terraform_connection_string
+	$(eval CONNECTION_URL := $(shell terraform output database_connection_url))
+endef
+
+define get_terraform_docker_registry_url
+	$(eval DOCKER_REGISTRY_URL := $(shell terraform output docker_repository_url))
+endef
+
+
 up:
 	@docker compose up
 build:
@@ -22,3 +31,32 @@ format:
 	@docker compose exec server bash -c "black ./src"
 get-from-api:
 	@docker compose exec http-client sh -c "curl -s \"http://server:8000$(path)\" | jq"
+terraform-plan:
+	@terraform plan -var-file="./project.tfvars"
+terraform-apply:
+	@terraform apply -var-file="./project.tfvars"
+terraform-refresh:
+	@terraform refresh -var-file="./project.tfvars"
+terraform-destroy:
+	@terraform destroy -var-file="./project.tfvars"
+run-migration-on-infra-database:
+	$(MAKE) terraform-refresh
+	$(call get_terraform_connection_string)
+	CONNECTION_URL=${CONNECTION_URL} docker compose -f ./docker-compose.database.deploy.yml up
+build-hosted-image:
+	docker build -f ./backend/Dockerfile.host ./backend -t hosted_book_server
+build-hosted-image-amd64:
+	docker build --platform="linux/amd64" -f ./backend/Dockerfile.host ./backend -t hosted_book_server
+run-server-image-pointing-to-database:
+	$(MAKE) build-hosted-image
+	$(MAKE) terraform-refresh
+	$(call get_terraform_connection_string)
+	docker run -p 8888:80 -e POSTGRES_URL=${CONNECTION_URL} hosted_book_server 
+upload-server-container-to-registry:
+	gcloud auth configure-docker
+	$(MAKE) build-hosted-image-amd64
+	$(MAKE) terraform-refresh
+	$(call get_terraform_docker_registry_url)
+	docker tag hosted_book_server ${DOCKER_REGISTRY_URL}/hosted_book_server
+	docker push ${DOCKER_REGISTRY_URL}/hosted_book_server
+	
---
+++ main.tf
@@ -0,0 +1,118 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+}
+
+variable "db_password" {
+  description = "The database password"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_project_service" "container_registry" {
+  project = var.project_id
+  service = "containerregistry.googleapis.com"
+}
+
+resource "google_project_service" "cloud_run_admin" {
+  project = var.project_id
+  service = "run.googleapis.com"
+}
+
+resource "google_sql_database_instance" "main_instance" {
+  name             = "main-postgresql-instance"
+  database_version = "POSTGRES_13"
+  region           = "us-central1"
+  settings {
+    tier = "db-f1-micro" 
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+         name = "all_networks"
+         value = "0.0.0.0/0"
+      }
+    }
+  }
+}
+
+resource "google_sql_database" "main_database" {
+  name     = "main-database"
+  instance = google_sql_database_instance.main_instance.name
+  charset  = "UTF8"
+  collation = "en_US.UTF8"
+}
+
+resource "google_sql_user" "postgre_user" {
+  name     = "postgresql_user"
+  instance = google_sql_database_instance.main_instance.name
+  password = var.db_password
+}
+
+data "google_container_registry_repository" "docker_repository" {
+}
+
+resource "google_cloud_run_service" "python_service" {
+  name     = "python-run-service"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = format("%s/%s:latest",
+          data.google_container_registry_repository.docker_repository.repository_url,
+          "hosted_book_server"
+        )
+        ports {
+          container_port = 80
+        }
+        env {
+          name = "POSTGRES_URL"
+          value = format("postgresql://%s:%s@%s/%s",
+            google_sql_user.postgre_user.name,
+            google_sql_user.postgre_user.password,
+            google_sql_database_instance.main_instance.ip_address[0].ip_address,
+            google_sql_database.main_database.name
+          )
+        }
+      }
+    }
+  }
+  depends_on = [ google_project_service.cloud_run_admin ]
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.python_service.location
+  project     = google_cloud_run_service.python_service.project
+  service     = google_cloud_run_service.python_service.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+  depends_on = [ google_cloud_run_service.python_service ]
+}
+
+output "docker_repository_url" {
+  value = data.google_container_registry_repository.docker_repository.repository_url
+}
+
+output "database_connection_url" {
+  sensitive = true
+  value = format("postgresql://%s:%s@%s/%s",
+    google_sql_user.postgre_user.name,
+    google_sql_user.postgre_user.password,
+    google_sql_database_instance.main_instance.ip_address[0].ip_address,
+    google_sql_database.main_database.name)
+}
+
+output "python_service_url" {
+  value = google_cloud_run_service.python_service.status[0].url
+}
